f6cd08d485a1e20e8a6f3294206b54692712a9b7
MathQnADEV <mathqna.project@gmail.com>
2023-02-05 09:31:34 +0800

    menambahkan file 2 file baru

--- fungsi.js
+++ fungsi.js
@@ -2,3 +2,12 @@ console.log("fungsi pertama")
 
 const add = (a, b) => a + b
 console.log(add(1, 2))
+
+const ada = "hayyu"
+console.log(ada)
+
+// cara mengetahui adanya perubahan dalam file di git, pake git diff
+// cara keluar dari git diff yaitu tekan q pada keyboard ente
+// dan git diff hanya melihat perubahan dalam file, bukan melihat adanya tambahan file
+// working directory -> staged area -> repository
+// jadi dari git init -> git add . -> git commit -> 
